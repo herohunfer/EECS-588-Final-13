e999a7a3d29e8bd2863cb19b4f8f64fc3e1d7edf
Ruoran Wang <ruoranwang@gmail.com>
2013-04-12 14:19:04 -0400

    ajax in copy.js to post image

--- copy-plugin/copy.js
+++ copy-plugin/copy.js
@@ -13,6 +13,13 @@ chrome.extension.onRequest.addListener(
            context.drawImage(image, request.left, request.top, request.width, request.height,0,0,request.width,request.height);
            //context.drawImage(image, 10, 10);
            var d = canvas.toDataURL('image/png');
+
+           ajax_post("http://127.0.0.1:5000/", {data: d}, function(data_text) {
+             //obj = toJson(txt)
+             //obj.name
+             alert("ok");
+           });
+
            alert("left:"+request.left
              +"\nwidth:"+request.width
              +"\ntop:"+request.top
@@ -26,3 +33,29 @@ chrome.extension.onRequest.addListener(
     }
     sendResponse({});
   });
+
+function ajax_post(url, data, callback) {
+  var httpRequest = new XMLHttpRequest();
+  var url = url;
+  var data = JSON.stringify(data);
+  var callback = callback;
+
+  var handler = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        // action
+        returned_text = httpRequest.responseText;
+        callback(returned_text);
+        // action
+      } else {
+        alert('There was a problem with the request. ' + httpRequest.status);
+      }
+    }
+  };
+
+  httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = handler;
+  httpRequest.open('POST', url);
+  httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  httpRequest.send('data=' + encodeURIComponent(data));
+}
